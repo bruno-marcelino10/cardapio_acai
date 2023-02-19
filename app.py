d7aa3aad114c818ebce9fae1443334ac2d8b20d1
Bruno Marcelino <brunomarcelino@ufmg.br>
2023-02-19 01:24:28 -0300

    criando outras páginas interativas

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def homepage():
-    return "Página Inicial"
+def produtos():
+    return render_template("index.html")
+
+@app.route("/unidades")
+def unidades():
+    return "Em construção"
+
+@app.route("/contato")
+def contato():
+    return "Em construção"
 
 if __name__ == "__main__":
     app.run()
